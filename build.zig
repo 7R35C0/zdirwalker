adad06a7b6ab4de894303ec2b76a80c34e843b9e
7R35C0 <7R35C0@proton.me>
2024-05-22 19:20:56 +0300

    update build.zig

--- build.zig
+++ build.zig
@@ -27,29 +27,42 @@ pub fn build(b: *std.Build) void {
     // Expose library module for later use with `@import("cfg.name")`
     const mod = setupModule(b, cfg);
 
-    // Build and install library (zig-out/lib)
+    // Build static library (zig-out/lib)
     const lib = setupLibrary(b, cfg);
 
-    // Run tests suite
+    // Run tests
     const tst = setupTest(b, cfg);
 
-    // Generate code coverage with kcov (zig-out/kcov)
+    // Generate code coverage (zig-out/cov)
+    // This function assumes that kcov (https://github.com/SimonKagstrom/kcov)
+    // is already installed on system.
+    // For code coverage report (zig-out/cov/index.html), use a live http server.
+    // The .vscode/extensions.json file contains an extension for this purpose.
     setupCoverage(b, tst);
 
-    // Generate documentation (zig-out/docs)
+    // Generate documentation (zig-out/doc)
+    // For documentation (zig-out/doc/index.html), same as above.
     setupDocumentation(b, lib);
 
     // Silent formatting zig files
     setupFormat(b);
 
-    // Remove cache directory (zig-cache)
-    setupRemoveCache(b);
+    // Remove specific directory (contain multiple steps):
+    //  * zig-cache
+    //  * zig-out
+    //  * zig-out/bin
+    //  * zig-out/doc
+    //  * zig-out/cov
+    //  * zig-out/lib
+    setupRemove(b);
 
-    // Remove output directory (zig-out)
-    setupRemoveOutput(b);
-
-    // Run specific example
-    setupExamples(b, cfg, mod);
+    // Run specific example (contain multiple steps):
+    //  * examples/cwddir
+    //  * examples/cwddir-oneup
+    //  * examples/exedir
+    //  * examples/exedir-oneup
+    //  * examples/exedir-twoup-onedown
+    setupExample(b, cfg, mod);
 }
 
 fn setupModule(b: *std.Build, cfg: Config) *std.Build.Module {
@@ -67,9 +80,15 @@ fn setupLibrary(b: *std.Build, cfg: Config) *std.Build.Step.Compile {
         .root_source_file = cfg.root_source_file,
         .version = cfg.version,
     });
-    const lib_install = b.addInstallArtifact(lib, .{});
+    const lib_install = b.addInstallArtifact(
+        lib,
+        .{},
+    );
 
-    const lib_step = b.step("lib", "Build and install library");
+    const lib_step = b.step(
+        "lib",
+        "Build static library   (zig-out/lib)",
+    );
     lib_step.dependOn(&lib_install.step);
 
     return lib;
@@ -77,7 +96,7 @@ fn setupLibrary(b: *std.Build, cfg: Config) *std.Build.Step.Compile {
 
 fn setupTest(b: *std.Build, cfg: Config) *std.Build.Step.Compile {
     const tst = b.addTest(.{
-        .name = "freetype-tests",
+        .name = cfg.name,
         .target = cfg.target,
         .optimize = cfg.optimize,
         .root_source_file = cfg.root_source_file,
@@ -85,7 +104,10 @@ fn setupTest(b: *std.Build, cfg: Config) *std.Build.Step.Compile {
     });
     const tst_run = b.addRunArtifact(tst);
 
-    const tst_step = b.step("test", "Run tests suite");
+    const tst_step = b.step(
+        "tst",
+        "Run tests",
+    );
     tst_step.dependOn(&tst_run.step);
 
     return tst;
@@ -96,22 +118,45 @@ fn setupCoverage(b: *std.Build, tst: *std.Build.Step.Compile) void {
         "kcov",
         "--clean",
         "--include-pattern=src/",
-        "zig-out/kcov",
+        "zig-cache/cov",
     });
     cov_run.addArtifactArg(tst);
 
-    const cov_step = b.step("kcov", "Code coverage with kcov");
-    cov_step.dependOn(&cov_run.step);
+    const cov_install = b.addInstallDirectory(.{
+        .install_dir = .{ .custom = "cov" },
+        .install_subdir = "",
+        .source_dir = .{
+            .src_path = .{
+                .owner = b,
+                .sub_path = "zig-cache/cov",
+            },
+        },
+    });
+    cov_install.step.dependOn(&cov_run.step);
+
+    const cov_remove = b.addRemoveDirTree(b.pathJoin(
+        &[_][]const u8{ b.cache_root.path.?, "cov" },
+    ));
+    cov_remove.step.dependOn(&cov_install.step);
+
+    const cov_step = b.step(
+        "cov",
+        "Generate code coverage (zig-out/cov)",
+    );
+    cov_step.dependOn(&cov_remove.step);
 }
 
 fn setupDocumentation(b: *std.Build, lib: *std.Build.Step.Compile) void {
     const doc_install = b.addInstallDirectory(.{
         .install_dir = .prefix,
-        .install_subdir = "docs",
+        .install_subdir = "doc",
         .source_dir = lib.getEmittedDocs(),
     });
 
-    const doc_step = b.step("docs", "Generate documentation");
+    const doc_step = b.step(
+        "doc",
+        "Generate documentation (zig-out/doc)",
+    );
     doc_step.dependOn(&doc_install.step);
 }
 
@@ -120,27 +165,63 @@ fn setupFormat(b: *std.Build) void {
         .paths = &.{
             "examples",
             "src",
+            "standalone",
             "build.zig",
             "build.zig.zon",
         },
         .check = false,
     });
 
-    const fmt_step = b.step("fmt", "Silent formatting zig files");
+    const fmt_step = b.step(
+        "fmt",
+        "Silent formatting",
+    );
     fmt_step.dependOn(&fmt.step);
 }
 
-fn setupRemoveCache(b: *std.Build) void {
-    const rmc_step = b.step("rmc", "Remove cache directory");
-    rmc_step.dependOn(&b.addRemoveDirTree("zig-cache").step);
+fn setupRemove(b: *std.Build) void {
+    const rmdir_cache_step = b.step(
+        "rm-cache",
+        "Remove cache           (zig-cache)",
+    );
+    rmdir_cache_step.dependOn(&b.addRemoveDirTree(b.cache_root.path.?).step);
+
+    const rmdir_out_step = b.step(
+        "rm-out",
+        "Remove output          (zig-out)",
+    );
+    rmdir_out_step.dependOn(&b.addRemoveDirTree(b.install_path).step);
+
+    const rmdir_bin_step = b.step(
+        "rm-bin",
+        "Remove binary          (zig-out/bin)",
+    );
+    rmdir_bin_step.dependOn(&b.addRemoveDirTree(b.exe_dir).step);
+
+    const rmdir_doc_step = b.step(
+        "rm-doc",
+        "Remove documentation   (zig-out/doc)",
+    );
+    rmdir_doc_step.dependOn(&b.addRemoveDirTree(b.pathJoin(
+        &[_][]const u8{ b.install_path, "doc" },
+    )).step);
+
+    const rmdir_cov_step = b.step(
+        "rm-cov",
+        "Remove code coverage   (zig-out/cov)",
+    );
+    rmdir_cov_step.dependOn(&b.addRemoveDirTree(b.pathJoin(
+        &[_][]const u8{ b.install_path, "cov" },
+    )).step);
+
+    const rmdir_lib_step = b.step(
+        "rm-lib",
+        "Remove library         (zig-out/lib)",
+    );
+    rmdir_lib_step.dependOn(&b.addRemoveDirTree(b.lib_dir).step);
 }
 
-fn setupRemoveOutput(b: *std.Build) void {
-    const rmo_step = b.step("rmo", "Remove output directory");
-    rmo_step.dependOn(&b.addRemoveDirTree("zig-out").step);
-}
-
-fn setupExamples(b: *std.Build, cfg: Config, mod: *std.Build.Module) void {
+fn setupExample(b: *std.Build, cfg: Config, mod: *std.Build.Module) void {
     var egs_dir = std.fs.openDirAbsolute(
         b.path("examples").getPath(b),
         .{ .iterate = true },
@@ -202,7 +283,7 @@ fn setupExamples(b: *std.Build, cfg: Config, mod: *std.Build.Module) void {
 
             const egs_step = b.step(
                 b.fmt("run-{s}", .{egs_name}),
-                b.fmt("Run '{s}' example", .{egs_name}),
+                b.fmt("Run example {s}", .{egs_name}),
             );
 
             egs_step.dependOn(&egs_run.step);
